f0c73a2c487fcca9d97a6abac6a43de40ca67183
Bin CHEN <hvnwnd@gmail.com>
2015-09-06 19:01:31 +0200

    Play video

--- FigFeed/FigFeed/Helper/FFHtmlGenerator.swift
+++ FigFeed/FigFeed/Helper/FFHtmlGenerator.swift
@@ -10,9 +10,15 @@ import Foundation
 
 class FFHtmlGenerator {
     
-    class func generateHtml(title: String, imageUrl:String,  subtitle: String,  content:String) -> String
+    class func generateHtml(title: String, imageUrl:String, videoUrl:String?, subtitle: String,  content:String) -> String
     {
-        let s = String(format: "<h1>%@</h1><img src='%@' width='360'><h3>%@</h3>%@", title, imageUrl, subtitle, content)
-        return s
+        var html:String
+        if videoUrl != nil {
+            html = String(format: "<h1>%@</h1><img src='%@' width='360'><h3>%@</h3><video width='320' height='240' controls><source src='%@' type='video/mp4'></video>%@", title, imageUrl, subtitle, videoUrl!, content)
+
+        }else {
+            html = String(format: "<h1>%@</h1><img src='%@' width='360'><h3>%@</h3>%@", title, imageUrl, subtitle, content)
+        }
+        return html
     }
 }
